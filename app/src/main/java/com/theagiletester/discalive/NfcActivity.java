3cd3491307c98f1783f13a54a06fecb332c59da1
Tim Walker <walketim@gmail.com>
2016-11-25 20:34:37 -0700

    working with some text prompts and the read tag information displayed in the itsalive page

--- app/src/main/java/com/theagiletester/discalive/NfcActivity.java
+++ app/src/main/java/com/theagiletester/discalive/NfcActivity.java
@@ -37,6 +37,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
 /**
  * An example of how to use the NFC foreground dispatch APIs. This will intercept any MIME data
  * based NDEF dispatch as well as all dispatched for NfcF tags.
@@ -48,7 +51,10 @@ public class NfcActivity extends Activity {
     private IntentFilter[] mFilters;
     private String[][] mTechLists;
     private TextView mText;
+    private TextView mTextView;
     private int mCount = 0;
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
 
     @Override
     public void onCreate(Bundle savedState) {
@@ -57,6 +63,8 @@ public class NfcActivity extends Activity {
         setContentView(activity_nfc);
 
         Log.d("Cucumber NFC", "nfcActivity onCreate with " + savedState);
+
+        mTextView = (TextView) findViewById(R.id.textViewNfcResult);
 
         mAdapter = NfcAdapter.getDefaultAdapter(this);
 
@@ -116,6 +124,13 @@ public class NfcActivity extends Activity {
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             Log.d("Cucumber NFC", "tag: " + tag);
 
+            if (MIME_TEXT_PLAIN.equals(type)) {
+                new NdefReaderTask().execute(tag);
+
+            } else {
+                Log.d("Cucumber NFC", "Wrong mime type: " + type);
+            }
+
         } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
 
             // In case we would still use the Tech Discovered Intent
@@ -123,17 +138,77 @@ public class NfcActivity extends Activity {
             String[] techList = tag.getTechList();
             String searchedTech = Ndef.class.getName();
             Log.d("Cucumber NFC", "tag: " + tag.toString());
-
-//            for (String tech : techList) {
-//                if (searchedTech.equals(tech)) {
-//                    new NdefReaderTask().execute(tag);
-//                    break;
-//                }
-//            }
+            for (String tech : techList) {
+                Log.d("Cucumber NFC", "tech: " + tech + ", searched: " + searchedTech);
+                if (searchedTech.equals(tech)) {
+                    new NdefReaderTask().execute(tag);
+                    break;
+               }
+           }
         }
     }
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
 
+        @Override
+        protected String doInBackground(Tag... params) {
+            Tag tag = params[0];
 
+            Ndef ndef = Ndef.get(tag);
+            if (ndef == null) {
+                // NDEF is not supported by this Tag.
+                return null;
+            }
 
+            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
 
+            NdefRecord[] records = ndefMessage.getRecords();
+            for (NdefRecord ndefRecord : records) {
+                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                    try {
+                        String theText = readText(ndefRecord);
+                        Log.d("Cucumber NFC", "theText: " + theText);
+                        return theText;
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e("Cucumber NFC", "Unsupported Encoding", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+            byte[] payload = record.getPayload();
+
+            // Get the Text Encoding
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+
+            // Get the Language Code
+            int languageCodeLength = payload[0] & 0063;
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                mTextView.setText(result);
+                Log.d("Cucumber NFC",result);
+            }
+        }
+    }
 }
